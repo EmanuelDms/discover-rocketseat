03e6f58204811ab59035c60312cecb7d8dc27248
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-11-03 08:22:37 -0300

    feat: create renames tables

--- sql/school.sql
+++ sql/school.sql
@@ -5,18 +5,24 @@ CREATE TABLE IF NOT EXISTS aluno (
   nome TEXT NOT NULL,
   responsavel TEXT
 );
-CREATE TABLE IF NOT EXISTS professores(
+CREATE TABLE IF NOT EXISTS professor(
   id SERIAL PRIMARY KEY,
   cpf INTEGER UNIQUE NOT NULL,
   nome TEXT NOT NULL,
   materia TEXT
 );
-CREATE TABLE IF NOT EXISTS aulas(
+CREATE TABLE IF NOT EXISTS aula(
   id_professor INTEGER NOT NULL,
   matricula_aluno INTEGER,
-  FOREIGN KEY (id_professor) REFERENCES professores(id),
+  FOREIGN KEY (id_professor) REFERENCES professor(id),
   FOREIGN KEY (matricula_aluno) REFERENCES aluno(matricula)
 );
 -- Rename table aluno to alunos
 ALTER TABLE aluno
   RENAME TO alunos;
+-- Rename table aula to aulas
+ALTER TABLE aula
+  RENAME TO aulas;
+-- Rename table professor to professores
+ALTER TABLE professor
+  RENAME TO professores;
